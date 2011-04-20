c32f4b05e2d27ac7400cae518883fb8ad69190d4
Andrew Kelley <superjoe30@gmail.com>
2011-04-20 00:39:08 -0700

    handle kicks, provide /list to n00bs, and latest gibberish

--- mcserve.py
+++ mcserve.py
@@ -7,6 +7,13 @@ mcserver = subprocess.Popen(['java', '-Xmx1024M', '-Xms1024M', '-jar', 'minecraf
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+def html_filter(in_txt):
+    filtered = in_txt.replace('&', '&amp;')
+    filtered = filtered.replace('"', '&quot;')
+    filtered = filtered.replace('<', '&lt;')
+    filtered = filtered.replace('>', '&gt;')
+    return filtered
+
 class GoodServer(HTTPServer):
     def __init__(self, server_address, handler):
         super().__init__(server_address, handler)
@@ -38,11 +45,16 @@ Content-type: text/html
         if len(onliners) > 0:
             self.wfile.write(bytes("<h2>Online players:</h2><ul>", 'utf8'))
             for onliner in onliners:
-                self.wfile.write(bytes("<li>{0}</li>".format(onliner), 'utf8'))
+                self.wfile.write(bytes("<li>{0}</li>".format(html_filter(onliner)), 'utf8'))
 
             self.wfile.write(bytes("</ul>", 'utf8'))
         else:
             self.wfile.write(bytes("<p>Nobody is online :-(</p>", 'utf8'))
+
+        self.wfile.write(bytes("<h2>latest gibberish</h2>", 'utf8'))
+        for date, name, msg in chat_msgs:
+            self.wfile.write(bytes("{0} &lt;{1}&gt; {2} <br/>".format(html_filter(date), html_filter(name), html_filter(msg)), 'utf8'))
+
 
 
         self.wfile.write(bytes("""
@@ -65,6 +77,10 @@ def run_read_text():
         line = full_line.strip()
         text_queue.put(line.decode('utf8'))
 
+def run_input():
+    while True:
+        line = input()
+        put_text(line)
 
 import threading
 
@@ -76,12 +92,22 @@ read_thread = threading.Thread(target=run_read_text, name="read")
 read_thread.daemon = True
 read_thread.start()
 
-onliners = set()
+input_thread = threading.Thread(target=run_input, name="input")
+input_thread .daemon = True
+input_thread .start()
 
-login_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+) \[\/(\d+\.\d+.\d+.\d+:\d+)\] logged in with entity id (\d+) at \(.+\)$')
-logout_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+) lost connection: .+$')
+onliners = set()
+chat_msgs = []
+
+login_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+) \[\/(\d+\.\d+.\d+.\d+:\d+)\] logged in with entity id (\d+?) at \(.+?\)$')
+logout_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+?) lost connection: (.+)$')
+kicked_float_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[WARNING\] (.+?) was kicked for (.+?)$')
+kicked_op_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+?)\: Kicking (.+?)$')
+chat_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] \<(.+?)\> (.+)$')
+op_command_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+?) issued server command\: (.+)$')
+nonop_command_re = re.compile(r'^(\d+\-\d+\-\d+ \d+\:\d+\:\d+) \[INFO\] (.+?) tried command\: (.+)$')
 def got_text(text):
-    print("got text from minecraft: {0}".format(text))
+    print("[out] {0}".format(text))
 
     groups = login_re.match(text)
     if groups is not None:
@@ -96,9 +122,57 @@ def got_text(text):
         except KeyError:
             pass
         print("{0} logged out".format(name))
+    groups = kicked_float_re.match(text)
+    if groups is not None:
+        name = groups.group(2)
+        why = groups.group(3)
+        try:
+            onliners.remove(name)
+        except KeyError:
+            pass
+        print("{0} kicked for {1}".format(name, why))
+    groups = kicked_op_re.match(text)
+    if groups is not None:
+        kicker = groups.group(1)
+        name = groups.group(2)
+        try:
+            onliners.remove(name)
+        except KeyError:
+            pass
+        print("{0} kicked by {1}".format(name, kicker))
+
+    groups = chat_re.match(text)
+    if groups is not None:
+        date = groups.group(1)
+        name = groups.group(2)
+        msg = groups.group(3)
+        chat_msgs.append((date, name, msg))
+        if len(chat_msgs) > 100:
+            chat_msgs.pop(0)
+
+    groups = op_command_re.match(text)
+    if groups is not None:
+        name = groups.group(2)
+        cmd = groups.group(3)
+        try_cmd(name, cmd, op=True)
+
+    groups = nonop_command_re.match(text)
+    if groups is not None:
+        name = groups.group(2)
+        cmd = groups.group(3)
+        try_cmd(name, cmd, op=False)
+
+def try_cmd(name, cmd, op=False):
+    print("try cmd '{0}' '{1}'".format(name, cmd))
+    if cmd == 'list':
+        if not op:
+            put_text("tell {0} {1}".format(name, ', '.join(onliners)))
+
 
 def put_text(text):
-    mcserver.stdin.write(bytes(text, 'utf8'))
+    print("[in] {0}".format(text))
+    mcserver.stdin.write(bytes(text+'\n', 'utf8'))
+    mcserver.stdin.flush()
 
 
 # main loop
